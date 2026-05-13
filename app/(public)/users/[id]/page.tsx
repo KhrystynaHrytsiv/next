2d42b6c5cc18980b55d4a9082bef1c58129fe5d9
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-05-13 12:34:02 +0300

    lesson 2 searchParams

--- app/(public)/users/[id]/page.tsx
+++ app/(public)/users/[id]/page.tsx
@@ -1,8 +1,11 @@
 import {FC} from "react";
 import {Metadata} from "next";
+import {SearchParams} from "next/dist/server/request/search-params";
+import {IUser} from "@/src/interfaces/IUser";
 
 type Props ={
     params:Promise<{id:string}>,
+    searchParams:Promise<SearchParams>
 }
 export const generateMetadata = async ({params}:Props):Promise<Metadata> =>{
     const {id} = await params;
@@ -10,13 +13,18 @@ export const generateMetadata = async ({params}:Props):Promise<Metadata> =>{
         title: 'User page title' + id
     }
 }
-const UserPage:FC<Props> = async ({params}) => {
-    const {id} = await params;
-
+const UserPage:FC<Props> = async ({searchParams}) => {
+    const sp = await searchParams;
+    console.log(sp);
+    const {data} = await searchParams;
+    let obj = null;
+    if (typeof data === 'string'){
+        obj =JSON.parse(data) as IUser;//for method with JSON
+    }
 
     return (
         <div>
-            user page: {id}
+            {obj && <>user page: {obj.id} {obj.name}</>}
         </div>
     );
 };
--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -7,7 +7,9 @@ const Users = async () => {
     return (
         <div>
             {users.map(user => <div key={user.id}>
-                <Link href={'/users/' +user.id.toString()}>{user.id} {user.name}</Link>
+                {/*<Link href={{pathname: '/users/'+user.id.toString(), query:{name:user.name}}}>{user.id} {user.name}</Link>*/}
+                {/*<Link href={{pathname: '/users/'+user.id.toString(), query:{...user}}}>{user.id} {user.name}</Link>*/}
+                <Link href={{pathname: '/users/'+user.id.toString(), query:{data:JSON.stringify(user)}}}>{user.id} {user.name}</Link>
             </div>)}
         </div>
     );
