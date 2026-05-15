d8ec1d9090badfcb5abe5248b804f423ecc4c536
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-05-15 12:59:40 +0300

    client zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-import Image from "next/image";
+'use client'
+import {useForm} from "react-hook-form";
+import {UserFormData, userSchema} from "@/lib/schema";
+import {zodResolver} from "@hookform/resolvers/zod";
 
 export default function Home() {
+    const {register, handleSubmit, formState:{errors}} = useForm<UserFormData>({resolver:zodResolver(userSchema)});
+
+    const onSubmit = async (data:UserFormData)=>{
+        const  res = await fetch('/api/users', {
+            method:'POST',
+            body: JSON.stringify(data)
+        });
+        const json = await res.json();
+        console.log(json);
+    }
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+              <input {...register('name')} placeholder={'name'}/>
+              {errors.name && (<p>{errors.name.message}</p>)}
+          </div>
+          <div>
+              <input {...register('email')} placeholder={'email'}/>
+              {errors.email && (<p>{errors.email.message}</p>)}
+          </div>
+          <div>
+              <input {...register('password')} placeholder={'password'}/>
+              {errors.password && (<p>{errors.password.message}</p>)}
+          </div>
+          <button type={'submit'}>Send</button>
+      </form>
+    </>
   );
 }
