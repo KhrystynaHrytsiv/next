1122c0f321042562ce6aeea0ba574e66b5c530e3
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-05-16 11:57:19 +0300

    lesson 3 SSG

---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+const UsersPage = async () => {
+    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+              .then(response => response.json());
+    return (
+        <div>
+            {users.map((user: {id:number, name:string}) => <div key={user.id}>{user.name}</div>)}
+        </div>
+    );
+};
+
+export default UsersPage;
